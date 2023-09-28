efe38c54296fe363038d96ef74f2176c0ec0c82a
FarexDev <146355652+FarexDev@users.noreply.github.com>
2023-09-28 16:23:19 +0200

    Sokac Loading V.1 Farex
    Uživajte..

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+//* Autor: Farex[Sokac Dev]
+version '1.0.0'
+
+files {
+    'index.html',
+    'pjesma.mp3',
+    'style.css',
+    'script.js',
+    'Cornerstone.ttf'
+}
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+
